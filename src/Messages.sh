98779b0d44b7fbaf5fcbdd99fd0e281dcb679997
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-12-21 15:49:04 +0100

    Fix i18n message catalog naming, add catalog for generator app
    Summary:
    * rust_qt_binding_generator uses tr() calls, but was missing
      catalog extraction and catalog loading
    * .pot is not part of the catalog name
    * Demo_qt is too generic name
    
    Reviewers: vandenoever
    
    Reviewed By: vandenoever
    
    Differential Revision: https://phabricator.kde.org/D9458

--- src/Messages.sh
+++ src/Messages.sh
@@ -3,4 +3,4 @@
 # Extract strings from all source files.
 # EXTRACT_TR_STRINGS extracts strings with lupdate and convert them to .pot with
 # lconvert.
-$EXTRACT_TR_STRINGS `find demo -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml` -o $podir/Demo_qt.pot
+$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h` -o $podir/rust_qt_binding_generator_qt.pot
