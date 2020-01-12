572893e09bca0da03ce801132b6745aa812d9eab
Jos van den Oever <jos@vandenoever.info>
2020-01-12 22:22:27 +0100

    Fix warning about overloaded

--- demo/src/SortedModel.h
+++ demo/src/SortedModel.h
@@ -28,8 +28,8 @@ class SortedModel : public QSortFilterProxyModel {
 Q_OBJECT
 public:
     SortedModel() :QSortFilterProxyModel() {}
-    bool filterAcceptsRow(int source_row, const QModelIndex & source_parent) const;
-    Q_INVOKABLE QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const {
+    bool filterAcceptsRow(int source_row, const QModelIndex & source_parent) const override;
+    Q_INVOKABLE QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override {
         return QSortFilterProxyModel::data(index, role);
     }
 public slots:
